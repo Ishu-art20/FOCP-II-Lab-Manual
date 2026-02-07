12425b7c2bc8ccd67f84357ee5a576c9a08ee288
Ishu <cishuu208206@gmail.com>
2026-02-07 21:30:20 +0530

    Prtactice

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,17 +1,38 @@
-// prime number 
+// armstrong and perfect Number
 #include<iostream>
+#include<cmath>
 using namespace std;
-int main(){
-    int n,flag=0;
-    cout<<"Enter number : ";
-    cin>>n;
-    for(int i=2;i<=n/2;i++){
-        if(n%i==0)
-        flag = 1;
-    } 
-     if(flag==1)
-        cout<<"Not a prime number";
-        else
-        cout<<"Prime number"; 
+int main()
+{ 
+    int no,count=0,copy;
+    int rem,rev=0,arm=0,sum=0;
+    cout<<"Enter number: ";
+    cin>>no;
+    copy= no;
+    count=0;
+    while(no>0)
+    {   count++;
+        no=no/10;    
+    }
+    no=copy;
+    while(no>0)
+    {
+        rem=no%10;
+        arm+=static_cast<int>(pow(rem,count));
+        no=no/10;
+    }
+    if(copy==arm)
+       cout<<"\nIt's an Armstrong Number"; 
+    else
+       cout<<"\nIt's not an Armstrong Number"; 
+    no=copy;
+    for(int i=1;i<no;i++){
+        if(no%i==0)
+        sum+=i;
+    }
+    if(sum==no)
+      cout<<"\nIt's a Perfect Number";
+    else 
+      cout<<"\nIt's not a Perfect Number";
     return 0;
-}  
+} 
